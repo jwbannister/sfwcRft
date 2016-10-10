e4be891589b447a70908d2f9e65175bfe454ee2c
John Bannister <jbannister@airsci.com>
2016-10-10 15:52:08 -0700

    updated scripts to add more transect comparisons and drying plot

--- scripts/sfwcrft_transects.R
+++ scripts/sfwcrft_transects.R
@@ -1,79 +1,66 @@
 load_all("~/code/owensData")
 library(tidyverse)
 library(ggplot2)
-
-query1 <- "SELECT * FROM sfwct.wetness_summary WHERE dcm='T26' AND day='2016-04-26'"
-df1 <- query_owenslake(query1)
-df2 <- filter(df1, trgtwet!=0, class!="D")
-df2$class <- ordered(df2$class, levels=c("M", "W"), 
-                     labels=c("Moist", "Wet"))
-df2$cover <- df2$round * 100
-
-label_df <- data.frame(trgtwet=c(45, 45, 55, 55, 65, 65, 75, 75), 
-                       pos=c(2.5, 20.5, 5.5, 33, 6, 42, 12.5, 39), 
-                       label=c("W = 5%", "M = 36%", "W = 11%", "M = 55%", 
-                               "W = 12%", "M = 72%", "W = 25%", "M = 53%"))
-
-swir_df <- data.frame(swir=c(24, 48, 69, 65), x=c(42, 52, 62, 72), 
-                      xend=c(48, 58, 68, 78))
-
-p1 <- df2[order(df2$class, decreasing=T), ] %>% 
-    ggplot(aes(x=trgtwet, y=cover)) +
-    geom_bar(stat="identity", aes(fill=class), color="black") +
-    scale_x_continuous(name="Target Wetness", breaks=c(45, 55, 65, 75), 
-                       labels=c("45%", "55%", "65%", "75%")) +
-    scale_y_continuous(name="", breaks=c(0, 20, 40, 60, 80, 100), 
-                       minor_breaks=seq(0, 100, 5), limits=c(0, 105), 
-                       expand=c(0, 0), labels=c("0%", "20%", "40%", "60%", 
-                                                "80%", "100%")) +
-    scale_fill_manual(name="Ground", values=c("#33a02c", "#0000f6")) +
-    geom_segment(data=swir_df, mapping=aes(x=x, xend=xend, y=swir, yend=swir, 
-                                           color="SWIR"), size=3) +
-    scale_color_manual(name="", values="red")
-png(filename="~/Desktop/t26_Apr26_areas.png", width=8, height=6, units="in", 
-    res=300)
-p1
-dev.off()
-
 library(rgdal)
-paths_df <- readOGR(path.expand("~/Desktop/presentation working files/temp/"), 
-                    "trans_example")@data
-path_lengths <- paths_df %>% group_by(buff) %>%
-    summarize(length=sum(length)) %>% ungroup()
-paths_sum <- paths_df %>% group_by(buff, class) %>%
-    summarize(length=sum(length)) %>% ungroup() %>%
-    left_join(path_lengths, by="buff") %>%
-    mutate(cover=round(length.x/length.y, 2))
-paths_cast <- paths_sum %>% select(-length.x, -length.y) %>%
-    spread(class, cover)
-paths_cast$MW <- paths_cast$M + paths_cast$W
-names(paths_cast)[2:5] <- tolower(paste0("grnd_", names(paths_cast)[2:5]))
-paths <- paths_cast %>% select(-grnd_d) %>%
-    gather(class, cover, 2:4)
-paths$cover <- paths$cover * 100
-paths$class <- ordered(paths$class, levels=c("grnd_w", "grnd_m", "grnd_mw"), 
-                     labels=c("Wet", "Moist", "MW"))
 
-swir_paths <- data.frame(x=c(0.7, 1.7, 2.7, 3.7, 4.7, 5.7), 
-                         xend=c(1.3, 2.3, 3.3, 4.3, 5.3, 6.3), 
-                         swir_w=c(77, 50, 75, 60, 37, 12)) 
+index <- c("1130", "1201", "0426", "0527")
+pth1 <- path.expand("~/dropbox/owens/sfwcrft/transect-swir compare/wet_paths/")
+pth2 <- path.expand("~/dropbox/owens/sfwcrft/transect-swir compare/transects/")
 
-plot_df <- filter(paths, class!="MW")
-p2 <- plot_df[order(plot_df$class, decreasing=F), ] %>% 
-    ggplot(aes(x=buff, y=cover)) +
-    geom_bar(stat="identity", aes(fill=class), color="black") +
-    scale_x_continuous(name="Transect", breaks=seq(1, 6, 1)) +
-    scale_y_continuous(name="", breaks=c(0, 20, 40, 60, 80, 100), 
-                       minor_breaks=seq(0, 100, 5), limits=c(0, 105), 
-                       expand=c(0, 0), labels=c("0%", "20%", "40%", "60%", 
-                                                "80%", "100%")) +
-    scale_fill_manual(name="Ground", values=c("#0000f6", "#33a02c")) +
-    geom_segment(data=swir_paths, mapping=aes(x=x, xend=xend, 
-                                              y=swir_w, yend=swir_w, 
-                                           color="SWIR"), size=3) +
-    scale_color_manual(name="", values="red")
-png(filename="~/Desktop/t26_Apr26_paths", width=8, height=6, units="in", 
-    res=300)
-p2
+for (i in index){
+    paths_df <- readOGR(pth2, paste0(i, "_wet_transect"))@data
+    path_lengths <- paths_df %>% group_by(id) %>%
+        summarize(length=sum(length)) %>% ungroup()
+    paths_sum <- paths_df %>% group_by(id, class) %>%
+        summarize(length=sum(length)) %>% ungroup() %>%
+        left_join(path_lengths, by="id") %>%
+        mutate(cover=round(length.x/length.y, 2))
+    paths_sum$cover <- paths_sum$cover * 100
+
+
+    swir_df <- readOGR(pth1, paste0(i, "_single"))@data
+    swir_areas <- swir_df %>% group_by(id) %>%
+        summarize(area=sum(area)) %>% ungroup()
+    swir_sum <- swir_df %>% group_by(id, DN) %>%
+        summarize(area=sum(area)) %>% ungroup() %>%
+        left_join(swir_areas, by="id") %>%
+        mutate(cover=round(area.x/area.y, 2)) %>%
+        filter(DN==100)
+    swir_sum$cover <- swir_sum$cover * 100
+
+    swir_plt <- data.frame(x=c(), xend=c(), swir_w=c())
+    for (j in unique(swir_sum$id)){
+        temp  <- data.frame(x=j-0.3, xend=j+0.3, 
+                            swir_w=filter(swir_sum, id==j)$cover)
+        swir_plt <- rbind(swir_plt, temp)
+    }
+    plot_df <- filter(paths_sum, class!="D")
+    plot_df$class <- ordered(plot_df$class, levels=c("M", "W"), 
+                             labels=c("Moist", "Wet"))
+
+    p2 <- plot_df[order(plot_df$class, decreasing=T), ] %>% 
+        ggplot(aes(x=id, y=cover)) +
+        geom_bar(stat="identity", aes(fill=class), color="black") +
+        scale_x_continuous(name="Transect", breaks=seq(1, max(plot_df$id), 1)) +
+        scale_y_continuous(name="", breaks=c(0, 20, 40, 60, 80, 100), 
+                           minor_breaks=seq(0, 100, 5), limits=c(0, 105), 
+                           expand=c(0, 0), labels=c("0%", "20%", "40%", "60%", 
+                                                    "80%", "100%")) +
+scale_fill_manual(name="Ground", values=c("#33a02c", "#0000f6")) +
+geom_segment(data=swir_plt, mapping=aes(x=x, xend=xend, 
+                                        y=swir_w, yend=swir_w, 
+                                        color="SWIR"), size=3) +
+scale_color_manual(name="", values="red") +
+ggtitle(i)
+png(filename=paste0("~/dropbox/owens/sfwcrft/transect-swir compare/output/",
+                    i, "_plot.png"), width=8, height=6, units="in", res=300)
+print(p2)
 dev.off()
+write.csv(swir_sum, 
+          file=paste0("~/dropbox/owens/sfwcrft/transect-swir compare/output/",
+                      i, "_swir.csv"))
+write.csv(paths_sum, 
+          file=paste0("~/dropbox/owens/sfwcrft/transect-swir compare/output/",
+                      i, "_transect.csv"))
+}
 
--- scripts/swir_drying.R
+++ scripts/swir_drying.R
@@ -20,24 +20,13 @@ ras_uniform[ , ] <- 1
 # sfwct_ras <- ras_clip(ras_uniform, sfwct_areas)
 sfwct_trgtwet_ras <- ras_clip(ras_uniform, sfwct_areas, fld="index")
 
-# for which dates are SWIR images available?
-# do not use May 9, 2015 (cloud cover) (June 8, 2015 will be used for May 2015 
-# wetness estimate)
-date_index <- c("041615", "060815", "062015", "113015", "120115", 
-                "042616", "052716", "062416")
-
-# get list of file names for SWIR images
-a <- list.files(path="~/dropbox/data/swir/LS_adjusted/", 
-                pattern=".tif")
-
-wetness <- vector(mode="list", length=length(date_index))
-names(wetness) <- date_index
-for (k in date_index){
+index <- c("1st", "2nd", "3rd")
+wetness <- vector(mode="list", length=length(index))
+names(wetness) <- index
+for (k in index){
   print(k)
-  fn <- a[substr(a, 1, 6)==k][1]
-  ras_swir <- raster(paste0("~/dropbox/data/swir/LS_adjusted/", fn))
-#  ras_uniform <- ras_swir
-#  ras_uniform[ , ] <- 1
+  fn <- paste0("0624_", k, ".tif")
+  ras_swir <- raster(paste0("~/dropbox/data/swir/LS_adjusted/drying/", fn))
   zones <- vector(mode="list", length=length(sfwct_areas@data$index))
   names(zones) <- sfwct_areas@data$index
   for (i in names(zones)){
@@ -60,67 +49,19 @@ for (k in date_index){
   }
 }
 
-# tag early June swir flight to May wetness estimate (late June flight will be 
-# used for June estimate
-names(wetness)[names(wetness)=='060815'] <- '05xx15'
-names(wetness) <- paste0(substr(names(wetness), 1, 2), 
-                         substr(names(wetness), 5, 6))   
+wetness[[1]]$flight <- 1
+wetness[[2]]$flight <- 2
+wetness[[3]]$flight <- 3
 
-swir_wet_tbl <- data.frame(dca=wetness[[1]]$dca, 
-                          trgtwet=wetness[[1]]$trgtwet)
-for (i in names(wetness)){
-  names(wetness[[i]])[3] <- paste0("swir.", i)
-  swir_wet_tbl <- left_join(swir_wet_tbl, wetness[[i]], 
-                           by=c("dca", "trgtwet"))
-}
+dry_df <- rbind(wetness[[1]], wetness[[2]], wetness[[3]])
+dry_df$swir.wet <- dry_df$swir.wet * 100
 
-# pull transect wetness data to fill in missing SWIR months
-query1 <- "SELECT dcm as dca, trgtwet, day, round AS wet
-           FROM sfwct.wetness_summary 
-           WHERE class='W'"
-trans_summ <- query_owenslake(query1)
-# remove April 20, 2016 T26 transects, use April 26 instead to coincide with SWIR 
-# flight. Remove duplicated day in Jun for T10 65%
-trans_summ <- filter(trans_summ, !(day=='2016-04-20' & dca=='T26'))
-trans_summ <- filter(trans_summ, !(day=='2015-06-09' & dca=='T10' & trgtwet==65))
-# add in 0% wetness in T26 0% area for all transect days (this is what was 
-# measured, but since wetness transect has 0 length for these areas, it will not 
-# show up in summary view
-a <- unique(filter(trans_summ, dca=='T26')$day)
-tmp <- data.frame(dca=rep('T26', length(a)), trgtwet=rep(0, length(a)), 
-                  day=a, wet=rep(0, length(a)))
-tmp1 <- data.frame(dca='T26', trgtwet=45, day='2016-03-08', wet=0) 
-tran_record <- rbind(trans_summ, tmp, tmp1)
-tran_record$period <- paste0(substr(tran_record$day, 6, 7), 
-                             substr(tran_record$day, 3, 4))
-# apply June 8 transect to may 2015 period
-tran_record[tran_record$period=='0615', ]$period <- '0515'
-trans_sub <- filter(tran_record, !(period %in% names(wetness)))
-# apply January 28 transect wetness to both Jan 2016 and Feb 2016 period
-tmp <- trans_sub %>% filter(period=='0116') %>% dplyr::select(-period) %>%
-  mutate(period='0216')
-trans_sub <- rbind(trans_sub, tmp) %>% dplyr::select(-day) %>%
-  arrange(period, dca, trgtwet)
-trans_sub$period <- paste0("tran.", trans_sub$period)
+p1 <- dry_df %>% filter(trgtwet!=0) %>%
+    ggplot(aes(x=flight, y=swir.wet, group=trgtwet)) +
+    geom_path(aes(color=trgtwet)) +
+    facet_grid(dca ~ ., scales="free_y") +
+    scale_x_continuous(name="Approx. Hours Drying", breaks=c(1, 2, 3), 
+                       labels=c("0", "1.5", "4.5")) +
+    scale_y_continuous(name="SWIR Wetness", breaks=seq(0, 100, 10), 
+                       minor_breaks=seq(5, 95, 10)) 
 
-wet_record <- melt(swir_wet_tbl, id.vars=c("dca", "trgtwet"), 
-                   variable.name="period", value.name="wet") %>%
-              rbind(trans_sub) %>% rename(index=period) %>%
-              mutate(method=substr(index, 1, 4), period=substr(index, 6, 9)) %>%
-              dplyr::select(-index)
-wet_record$trgtwet <- as.integer(wet_record$trgtwet)
-wet_record$period <- ordered(wet_record$period, 
-                             levels=c('0415', '0515', '0615', '1115', '1215',
-                                      '0116', '0216', '0316', '0416', '0516',
-                                      '0616'))
-full_record <- expand.grid(period=unique(wet_record$period), 
-                           dca=unique(wet_record$dca), 
-                           trgtwet=unique(wet_record$trgtwet)) %>%
-               filter(!(dca=='T13' & !(trgtwet %in% c(55, 75))), 
-                      !(dca=='T29' & !(trgtwet %in% c(0, 45))))               
-wet_record <- left_join(full_record, wet_record, 
-                        by=c("dca", "trgtwet", "period")) %>%
-              arrange(dca, period, trgtwet)
-wet_record <- wet_record[!(duplicated(wet_record)), ]
-
-save(wet_record, tran_record, file="./data/wetness.RData")
